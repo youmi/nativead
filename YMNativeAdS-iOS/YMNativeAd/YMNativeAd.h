957f633eff4faaa58ff464cf414db35ed6b04f74
caizhitao <caizhitao@youmi.net>
2017-06-30 15:30:15 +0800

    整理YmNativeAd的代码注释

--- YMNativeAdS-iOS/YMNativeAd/YMNativeAd.h
+++ YMNativeAdS-iOS/YMNativeAd/YMNativeAd.h
@@ -9,70 +9,78 @@
 #import <Foundation/Foundation.h>
 #import "UMNDataModel.h"
 
-
-@protocol YMNativeAdDelegate <NSObject>
+/**
+ 广告请求结果代理
+ */
+@protocol YMNativeAdDelegate < NSObject >
 
 @required
 
 /**
- * 获取广告成功，返回UMNDataModel对象的数组
+ 获取广告成功后的回调
+
+ @param nativeAdArray 返回的广告数组UMNDataModel
  */
--(void)ymNativeAdSuccessToLoad:(NSArray *)nativeAdArray;
+- (void)ymNativeAdSuccessToLoad:(NSArray *)nativeAdArray;
 
 /**
- * 获取广告失败
+ 获取广告失败后的回调
+
+ @param error 对应的错误代码
  */
--(void)ymNativeAdFailedToLoad:(NSError *)error;
+- (void)ymNativeAdFailedToLoad:(NSError *)error;
 
 @end
-
 
 @interface YMNativeAd : NSObject
 
 /**
- *  委托对象
+ 委托对象
  */
-@property (nonatomic, weak) id<YMNativeAdDelegate> delegate;
+@property (nonatomic, weak) id< YMNativeAdDelegate > delegate;
 
 /**
- *  初始化方法
- *  appid   :应用id
- *  slotid  :广告位id
+ 初始化广告请求对象
+ 
+ @param appid 开发者应用的APPID
+ @param slotid 广告位
+ @return 广告请求对象
  */
--(instancetype)initWithAppId:(NSString *)appid slotId:(NSString *)slotid;
+- (instancetype)initWithAppId:(NSString *)appid slotId:(NSString *)slotid;
 
 /**
- *  设置广告位对应的内容
- *  title   :内容的标题
- *  kw      :内容的关键词，逗号分隔 
+ （可选）设置请求广告时附带的可选参数
+
+ @param title 内容标题
+ @param kw 内容关键词，逗号分隔
  */
--(void)setContTitle:(NSString *)title contKw:(NSString *)kw;
+- (void)setContTitle:(NSString *)title contKw:(NSString *)kw;
 
 /**
- * 获取广告
- * 一次返回一个
+ 异步请求广告，一次返回一个
  */
--(void)loadAd;
-
-
+- (void)loadAd;
 
 /**
- *  广告点击
- * 【必选】：用户点击广告调用本方法
- *  注：同一个广告请不要重复调用
+ 处理广告点击事件，在用户点击广告时，需要调用本方法，同一个广告不要重复调用
+
+ @param adData 待点击的广告数据
+ @param callBackBlock 完成有米广告点击逻辑后的回调block，开发者可以在block里处理自己的逻辑
  */
--(void)clickAd:(UMNDataModel *)adData callBackBlock:(void(^)(NSError *error))callBackBlock;
+- (void)clickAd:(UMNDataModel *)adData callBackBlock:(void (^)(NSError *error))callBackBlock;
 
 /**
- *  广告展示
- * 【必选】：展示广告调用本方法
- *  注：每个广告展示之前都必须调用本方法一次，请不要重复调用
+ 处理广告展示事件，在准备展示广告时，需要调用本方法，每个广告展示之前都必须调用本方法，但同一个广告不要重复调用本方法
+
+ @param adData 待展示的广告数据
+ @param callBackBlock 完成有米广告展示逻辑后的回调block，开发者可以在block里处理自己的逻辑
  */
 -(void)showAd:(UMNDataModel *)adData callBackBlock:(void(^)(NSError *error))callBackBlock;
 
 /**
- * 点击广告打开AppStore
- * adData   :被点击的广告
+ 执行广告的点击逻辑：具体一般为打开广告所对应的APPStore
+
+ @param adData 被点击的广告数据
  */
 -(void)clickAdOpenAppStoreVC:(UMNDataModel*)adData;
 @end
