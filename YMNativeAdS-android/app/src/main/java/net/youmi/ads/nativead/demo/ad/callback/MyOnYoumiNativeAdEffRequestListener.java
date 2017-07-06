904d29dec96da550b5cf2cf245f09582ac7d33af
caizhitao <caizhitao@youmi.net>
2017-07-06 17:30:39 +0800

    update demo

--- YMNativeAdS-android/app/src/main/java/net/youmi/ads/nativead/demo/ad/callback/MyOnYoumiNativeAdEffRequestListener.java
+++ YMNativeAdS-android/app/src/main/java/net/youmi/ads/nativead/demo/ad/callback/MyOnYoumiNativeAdEffRequestListener.java
@@ -24,7 +24,7 @@ public class MyOnYoumiNativeAdEffRequestListener implements OnYoumiNativeAdEffRe
 	}
 	
 	/**
-	 * 效果记录发送完毕时回调
+	 * 效果记录发送完毕时回调(本回调方法执行在UI线程中)
 	 *
 	 * @param isSuccess 是否所有对应的效果记录都发送成功
 	 * @param adModel   效果记录对应的广告信息对象
